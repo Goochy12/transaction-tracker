cb5dcd39a48b3dbee8856b5ac79e6ebdbab7ba4d
Goochy12-Laptop <liamgooch@live.com.au>
2023-01-14 18:01:20 +1100

    Update Load_tblStg_Transactions.sql

--- database_files/procedures/Load_tblStg_Transactions.sql
+++ database_files/procedures/Load_tblStg_Transactions.sql
@@ -1,11 +1,13 @@
 CREATE PROCEDURE `Load_tblStg_Transactions` ()
 BEGIN
-insert into tblStg_Transactions (date, cost, description)
+insert into tblStg_Transactions (id, date, cost, description, meta_hash)
 (
 	SELECT 
-    STR_TO_DATE(date, "%d/%m/%Y") AS date
+    id
+    , STR_TO_DATE(date, "%d/%m/%Y") AS date
     , CONVERT(cost, DECIMAL(12,2)) AS cost
     , description
+    , MD5(CONCAT(date, cost, description)) AS meta_hash
     FROM tblLnd_Transactions
 );
 END
