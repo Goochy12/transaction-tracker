b0aa2c631b50502e5289dec8b1616538b04b1766
Goochy12-Laptop <liamgooch@live.com.au>
2023-01-14 17:05:19 +1100

    Update Load_TF_Transactions.sql

--- database_files/procedures/Load_TF_Transactions.sql
+++ database_files/procedures/Load_TF_Transactions.sql
@@ -2,12 +2,12 @@ CREATE PROCEDURE `Load_TF_Transactions` ()
 BEGIN
 INSERT INTO TF_Transactions (id, TD_Salary_id, TD_Rent_id, date, cost, description)
 SELECT
-id
+tf.id
 , IFNULL(salary.id, -1) AS TD_Salary_id
 , IFNULL(rent.id, -1) AS TD_Rent_id
-, date
-, cost
-, description
+, tf.date
+, tf.cost
+, tf.description
 FROM tmp_TF_Transactions tf
 LEFT JOIN TD_Salary salary
 ON tf.date between salary.effective_from AND salary.effective_to
